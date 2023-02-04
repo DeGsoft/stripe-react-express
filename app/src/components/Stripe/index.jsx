b640fd62a1d2f8fc1811e6979a2351524d65e753
DeGsoft <diegoezequielguillen@gmail.com>
2023-02-04 19:56:15 -0300

    [stripe] feat (Stripe): CheckoutForm

--- app/src/components/Stripe/index.jsx
+++ app/src/components/Stripe/index.jsx
@@ -1,45 +1,49 @@
-import {useStripe, useElements, PaymentElement} from '@stripe/react-stripe-js';
+import styles from './index.module.css';
+import React, { useState, useEffect } from "react";
+import {Elements} from '@stripe/react-stripe-js';
+import {loadStripe} from '@stripe/stripe-js';
+import CheckoutForm from './CheckoutForm.jsx';
 
-const CheckoutForm = () => {
-  const stripe = useStripe();
-  const elements = useElements();
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+const STRIPE_PK = process.env.STRIPE_PK || 'pk_test_51MXm0bAbd5y5rSxy9Z95DQcLi2OI0NNqAuTlrCWmxgQOndYPQw2rl9e6n6u5N0guFMBUpGMibrvbaIBDjUoOHaRH00pe9bBDpb';
+const stripePromise = loadStripe(STRIPE_PK);
 
-  const handleSubmit = async (event) => {
-    // We don't want to let default form submission happen here,
-    // which would refresh the page.
-    event.preventDefault();
+export default function Stripe() {
 
-    if (!stripe || !elements) {
-      // Stripe.js has not yet loaded.
-      // Make sure to disable form submission until Stripe.js has loaded.
-      return;
-    }
+  const [clientSecret, setClientSecret] = useState("");
 
-    const result = await stripe.confirmPayment({
-      //`Elements` instance that was used to create the Payment Element
-      elements,
-      confirmParams: {
-        return_url: "https://example.com/order/123/complete",
-      },
-    });
+  useEffect(() => {
+    // Create PaymentIntent as soon as the page loads
+    fetch("/create-payment-intent", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret));
+  }, []);
 
+  // const options = {
+  //   // passing the client secret obtained from the server
+  //   clientSecret: '{{CLIENT_SECRET}}',
+  // };
 
-    if (result.error) {
-      // Show error to your customer (for example, payment details incomplete)
-      console.log(result.error.message);
-    } else {
-      // Your customer will be redirected to your `return_url`. For some payment
-      // methods like iDEAL, your customer will be redirected to an intermediate
-      // site first to authorize the payment, then redirected to the `return_url`.
-    }
+  const appearance = {
+    theme: 'stripe',
+  };
+  const options = {
+    clientSecret,
+    appearance,
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <PaymentElement />
-      <button disabled={!stripe}>Submit</button>
-    </form>
-  )
+    <div className={styles}>
+      {clientSecret && (
+        <Elements options={options} stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
+      )}
+    </div>
+  );
 };
-
-export default CheckoutForm;
