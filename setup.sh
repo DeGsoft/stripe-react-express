582f6ea33627b918295c4f4a0454240f80ae675d
DeGssoft <diegoezequielguillen@gmail.com>
2023-02-04 10:03:05 -0300

    [dev] feat (setup): components

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
 nvm use 18.13.0
 npx create-react-app app
 cd app
+npm i -E --save @stripe/react-stripe-js @stripe/stripe-js
+mkdir src/components
+mkdir src/components/Stripe
+touch src/components/Stripe/index.jsx
